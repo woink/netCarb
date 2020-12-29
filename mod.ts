1e305681b8b5122d824a3b5bdaacea182019ded9
woink <wardprice@gmail.com>
2020-12-29 00:46:40 -0500

    initial funtionality

--- mod.ts
+++ mod.ts
@@ -1,7 +1,8 @@
-interface IResults {
-  id: number
-  name: string
-  image: string
+interface INutrients {
+  title: string
+  amount: number
+  unit: string
+  percentOfDailyNeeds: number
 }
 
 const ingredient = prompt("What ingredient are you looking up?")
@@ -17,11 +18,17 @@ const ingreSearch = `https://api.spoonacular.com/food/ingredients/search?query=$
 const resp = await fetch(ingreSearch)
 const data = await resp.json()
 
-const ingreID = data.results[0].id
+const ingreDetails = data.results[0]
 
-const ingreInfo = `https://api.spoonacular.com/food/ingredients/${ingreID}/information?amount=1&apiKey=${KEY}`
+const ingreInfo = `https://api.spoonacular.com/food/ingredients/${ingreDetails.id}/information?amount=1&apiKey=${KEY}`
 
 const respInfo = await fetch(ingreInfo)
 const dataInfo = await respInfo.json()
 
-console.log(dataInfo)
+const ingreNetCarb = dataInfo.nutrition.nutrients.find((el: INutrients) => el.title === "Net Carbohydrates")
+const ingreSugar = dataInfo.nutrition.nutrients.find((el: INutrients) => el.title === "Sugar")
+
+
+console.log(`${ingreDetails.name}`)
+console.log(`${ingreNetCarb.amount}${ingreNetCarb.unit} ${ingreNetCarb.title}`)
+console.log(`${ingreSugar.amount}${ingreSugar.unit} ${ingreSugar.title}`)
